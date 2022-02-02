dbb5e8b48aee09dac3d33fcb1e8cb5f62f9699e5
ran1024 <ran1024@yandex.ru>
2022-02-02 17:42:13 +1000

    Исправлена обработка ошибок

--- ug_active_ip/src/view_lic_ip.py
+++ ug_active_ip/src/view_lic_ip.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
+#
+# ug_active_ip (Gets the list of IP addresses that hold the NGFW license).
+#
+# Copyright @ 2021-2022 UserGate Corporation. All rights reserved.
+# Author: Aleksei Remnev <ran1024@yandex.ru>
+# License: GPLv3
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact the site <https://www.gnu.org/licenses/>.
+#
 #########################################################################################
-# Версия 0.3                                                                            #
+# Версия 1.0                                                                            #
 # Программа выводит число активных коннектов и список IP-адресов, занимающих лицензию.  #
 #########################################################################################
 
